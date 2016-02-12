aba900b4ba7e6dbcf7b30eb1585c273e0e6362e3
Brent Allen <labstudios@gmail.com>
2016-02-11 17:48:27 -0700

    before week 4 class

---
+++ Assets/_CompletedAssets/Scripts/Player/PlayerWeaponManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace CompleteProject{
+	public class PlayerWeaponManager : MonoBehaviour {
+
+		private const int NORMAL_DAMAGE = 20;
+		private const int SPREAD_DAMAGE = 15;
+
+		public GameObject leftBarrel;
+		public GameObject centerBarrel;
+		public GameObject rightBarrel;
+
+		private PlayerShooting leftShot;
+		private PlayerShooting centerShot;
+		private PlayerShooting rightShot;
+
+		private static PlayerWeaponManager player;
+
+		// Use this for initialization
+		void Awake () {
+			player = this;
+
+			leftShot = leftBarrel.GetComponent<PlayerShooting>();
+			centerShot = centerBarrel.GetComponent<PlayerShooting>();
+			rightShot = rightBarrel.GetComponent<PlayerShooting>();
+
+			resetWeapon();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		private void resetWeapon(){
+
+			leftShot.enabled = false;
+			rightShot.enabled = false;
+
+			centerShot.damagePerShot = NORMAL_DAMAGE;
+		}
+
+		public static void activateSpread(){
+			player.enableSpread();
+		}
+
+		public void enableSpread(){
+
+			leftShot.enabled = true;
+			rightShot.enabled = true;
+
+			leftShot.damagePerShot = SPREAD_DAMAGE;
+			rightShot.damagePerShot = SPREAD_DAMAGE;
+			centerShot.damagePerShot = SPREAD_DAMAGE;
+
+		}
+	}
+}
---
+++ Assets/_CompletedAssets/Scripts/Weapons/SpreadWeapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace CompleteProject{
+	public class SpreadWeapon : MonoBehaviour {
+
+		void OnTriggerEnter(Collider col){
+			print("touching");
+
+			PlayerWeaponManager pwm = col.gameObject.GetComponent<PlayerWeaponManager>();
+
+			if(pwm != null){
+				//PlayerWeaponManager.activateSpread(); //static function
+				pwm.enableSpread();
+			}
+			
+		}
+	}
+}
